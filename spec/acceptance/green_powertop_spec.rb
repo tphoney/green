3ba280fa527a609d62366a09895dceb5c0080fc1
tphoney <tp@puppet.com>
2018-05-25 11:51:25 +0100

    (add) Use the pdk

--- spec/acceptance/green_powertop_spec.rb
+++ spec/acceptance/green_powertop_spec.rb
@@ -2,16 +2,16 @@ require 'spec_helper_acceptance'
 
 powertop_packagename = 'powertop'
 
-describe 'green::powertop class', :unless => UNSUPPORTED_PLATFORMS.include?(fact('osfamily')) do
+describe 'green::powertop class', unless: UNSUPPORTED_PLATFORMS.include?(fact('osfamily')) do
   it 'installs the package' do
-    apply_manifest(%{
+    apply_manifest(%(
       class { 'green': }
-    }, :catch_failures => true)
+    ), catch_failures: true)
   end
 
   Array(powertop_packagename).each do |package|
     describe package(package) do
-      it { should be_installed }
+      it { is_expected.to be_installed }
     end
   end
 end
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -2,31 +2,28 @@ require 'beaker-rspec'
 require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
 
-UNSUPPORTED_PLATFORMS = ['windows', 'Darwin']
+UNSUPPORTED_PLATFORMS = %w[windows Darwin].freeze
 
 run_puppet_install_helper
-install_ca_certs unless ENV['PUPPET_INSTALL_TYPE'] =~ /pe/i
+install_ca_certs unless ENV['PUPPET_INSTALL_TYPE'] =~ %r{pe}i
 install_module_on(hosts)
 install_module_dependencies_on(hosts)
 
-unless ENV['RS_PROVISION'] == 'no' or ENV['BEAKER_provision'] == 'no'
+unless ENV['RS_PROVISION'] == 'no' || ENV['BEAKER_provision'] == 'no'
 
   hosts.each do |host|
   end
 end
 
 RSpec.configure do |c|
-  # Project root
-  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-
   # Readable test descriptions
   c.formatter = :documentation
 
   # Configure all nodes in nodeset
   c.before :suite do
     hosts.each do |host|
-      #copy_module_to(host, :source => proj_root, :module_name => 'green')
-      on host, puppet('module','install','puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
+      # copy_module_to(host, :source => proj_root, :module_name => 'green')
+      on host, puppet('module', 'install', 'puppetlabs-stdlib'), acceptable_exit_codes: [0, 1]
     end
   end
 end
